fec0cb808f82fe5b4fa891e0f00c7ec9430048a2
syaiful505 <syaifulstyle505@gmail.com>
2020-12-14 00:43:43 +0700

    selesai

--- main.cpp
+++ main.cpp
@@ -121,7 +121,7 @@ void Logic()
     // if (x > width && || x < 0 || y > height || y < 0) //game over bila nabrak tembok
         // gameOver = true;
     if (x >= width) x = 0; else if (x < 0) x = width - 1;
-       if (y >= height) y = 0; else if (y < 0) y = height - 1;
+        if (y >= height) y = 0; else if (y < 0) y = height - 1;
     for (int i = 0; i < nTail; i++)
         if (tailX[i] == x && tailY[i] == y)
             gameOver = true;
