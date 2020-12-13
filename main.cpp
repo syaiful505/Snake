02dc51f12167c64b2b8c0f48d3e07673717e7fe0
syaiful505 <syaifulstyle505@gmail.com>
2020-12-14 00:15:38 +0700

    nabrak tembok

--- main.cpp
+++ main.cpp
@@ -90,6 +90,8 @@ void Logic()
         default:
             break;
     }
+    if (x > width && || x < 0 || y > height || y < 0) //game over bila nabrak tembok
+        gameOver = true;
 }
 int main()
 {
