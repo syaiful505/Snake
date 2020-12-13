fa4680378bf1920baf819bfc767b221c3e9056c4
syaiful505 <syaifulstyle505@gmail.com>
2020-12-13 23:50:13 +0700

    system

--- main.cpp
+++ main.cpp
@@ -9,10 +9,36 @@ eDirection dir;
 void Setup()
 {
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 void Draw()
 {
+    system("cls"); //system("clear");
+    for (int i = 0; i < width; i++)
+        cout << "#";
+            cout << endl;
 
+    for (int i=0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            else
+                cout << " ";
+            if (j == width-1)
+                cout << "#";
+        }
+    }
+
+    for (int i = 0; i < width; i++)
+        cout << "#";
+            cout << endl;
 }
 void Input()
 {
