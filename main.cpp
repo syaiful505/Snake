a4a84adfdc921ae3c525fee1643c38a1a5b688b0
syaiful505 <syaifulstyle505@gmail.com>
2020-12-13 23:44:22 +0700

    control

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@ using namespace std;
 bool gameOver;
 const int width = 20;
 const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection ( STOP = 0, LEFT, RIGHT, UP, DOWN);
+eDirection dir;
 void Setup()
 {
     gameOver = false;
