c15103b997dec21804fedcec6a592ece200b33a2
syaiful505 <syaifulstyle505@gmail.com>
2020-12-13 23:41:43 +0700

    map

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
 void Setup()
 {
-
+    gameOver = false;
 }
 void Draw()
 {
@@ -18,6 +21,13 @@ void Logic()
 }
 int main()
 {
-
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        // Sleep(10); sleep(10);
+    }
     return 0;
 }
